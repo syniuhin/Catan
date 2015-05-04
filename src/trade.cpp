55d90e8e9b3aab9c26cef6d00590da9d0db4cba0
infm <syniuhin@gmail.com>
2015-05-04 19:31:23 +0300

    Added resource cells

--- src/trade.cpp
+++ src/trade.cpp
@@ -28,14 +28,22 @@ TradeWindow* TradeWindow::CreateInstance() {
     TradeWindow* instance = new TradeWindow;
     instance -> shape_.setPosition(instance -> pos_);
     instance -> shape_.setFillColor(instance -> bg_color_);
+
+    for (int i = 0; i < 5; ++i) {
+        instance -> children_
+            .insert(ResourceCell::CreateInstance(i,
+                    instance -> pos_ +
+                        sf::Vector2f(10 +
+                            i * (RESOURCE_CELL_SIZE.x + 20),
+                        10)));
+    }
     return instance;
 }
 
 TradeWindow::TradeWindow()
     : UiObject((sf::Vector2f(SCREEN_WIDTH, SCREEN_HEIGHT) -
                 sf::Vector2f(TRADE_WINDOW_WIDTH, TRADE_WINDOW_HEIGHT)) * .5f),
-      shape_(sf::Vector2f(TRADE_WINDOW_WIDTH,
-                TRADE_WINDOW_HEIGHT)) {}
+      shape_(TRADE_WIN_SIZE) {}
 
 void TradeWindow::Draw(sf::RenderWindow* window) {
     window -> draw(shape_);
@@ -50,3 +58,43 @@ bool TradeWindow::OnMouse(sf::Vector2i cursor) const {
     return false;
 }
 
+ResourceCell* ResourceCell::CreateInstance(int res_id,
+            sf::Vector2f pos) {
+    ResourceCell* instance = new ResourceCell;
+    switch (res_id) {
+        case BRICKS_ID:
+            instance -> shape_.setFillColor(instance -> brick_color);
+            break;
+        case WOOL_ID:
+            instance -> shape_.setFillColor(instance -> wool_color);
+            break;
+        case ORE_ID:
+            instance -> shape_.setFillColor(instance -> ore_color);
+            break;
+        case GRAIN_ID:
+            instance -> shape_.setFillColor(instance -> grain_color);
+            break;
+        case LUMBER_ID:
+            instance -> shape_.setFillColor(instance -> lumber_color);
+            break;
+    }
+
+    instance -> pos_ = pos;
+    instance -> shape_.setPosition(pos);
+    return instance;
+}
+
+ResourceCell::ResourceCell()
+    : shape_(RESOURCE_CELL_SIZE) {}
+
+void ResourceCell::Draw(sf::RenderWindow* window) {
+    window -> draw(shape_);
+}
+
+void ResourceCell::Click() {}
+
+bool ResourceCell::OnMouse(sf::Vector2i cursor) const {
+    return shape_
+        .getGlobalBounds()
+        .contains((float) cursor.x, (float) cursor.y);
+}
--- src/trade.h
+++ src/trade.h
@@ -5,6 +5,12 @@
 
 #include <set>
 
+static const sf::Vector2f TRADE_WIN_SIZE =
+        sf::Vector2f(TRADE_WINDOW_WIDTH, TRADE_WINDOW_HEIGHT);
+
+static const sf::Vector2f RESOURCE_CELL_SIZE =
+        sf::Vector2f(TRADE_WIN_SIZE.x / 5 - 20, TRADE_WIN_SIZE.y - 20);
+
 class UiObject {
     public:
         virtual void Draw(sf::RenderWindow*);
@@ -21,6 +27,8 @@ class UiObject {
         UiObject(sf::Vector2f);
 };
 
+class ResourceCell;
+
 class TradeWindow : public UiObject {
     public:
         static TradeWindow* CreateInstance();
@@ -34,4 +42,25 @@ class TradeWindow : public UiObject {
         TradeWindow();
 };
 
+class ResourceCell : public UiObject {
+    public:
+        static ResourceCell* CreateInstance(int res_id, sf::Vector2f pos);
+
+        void Draw(sf::RenderWindow*);
+        void Click();
+        bool OnMouse(sf::Vector2i) const;
+    private:
+        sf::RectangleShape shape_;
+        const sf::Color brick_color = sf::Color(191, 89, 0, 255);
+        const sf::Color wool_color = sf::Color(232, 232, 232, 255);
+        const sf::Color ore_color = sf::Color(85, 85, 125, 255);
+        const sf::Color grain_color = sf::Color(235, 255, 107, 255);
+        const sf::Color lumber_color = sf::Color(14, 163, 0, 255);
+
+        sf::Font font_;
+        sf::Text text_give_;
+        sf::Text text_take_;
+
+        ResourceCell();
+};
 #endif
